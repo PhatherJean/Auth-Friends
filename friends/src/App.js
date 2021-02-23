8a88354173f962d9344f3142b655c95df8969254
Patrice Jean <73203091+PhatherJean@users.noreply.github.com>
2021-02-23 18:46:12 -0500

    CRA friends and install needed dependencies

---
+++ friends/src/App.js
@@ -0,0 +1,13 @@
+// import logo from "./logo.svg";
+import { Route, Link, Switch } from "react-router-dom";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Friends App</h1>
+    </div>
+  );
+}
+
+export default App;
