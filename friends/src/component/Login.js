f7d2bfd532c2f803adee62077a6524c978f934f4
Patrice Jean <73203091+PhatherJean@users.noreply.github.com>
2021-02-23 20:56:31 -0500

    Friend is built and rendering

---
+++ friends/src/component/Login.js
@@ -0,0 +1,67 @@
+import axios from "axios";
+import React from "react";
+
+class Login extends React.Component {
+  state = {
+    credentials: {
+      username: "",
+      password: "",
+    },
+    loading: false,
+    error: "",
+  };
+
+  handleChange = (e) => {
+    const { name, value } = e.target;
+    this.setState({
+      ...this.state,
+      credentials: {
+        ...this.state.credentials,
+        [name]: value,
+      },
+      error: "",
+    });
+  };
+
+  login = (e) => {
+    e.preventDefault();
+    axios
+      .post("http://localhost:5000/api/login", this.state.credentials)
+      .then((res) => {
+        localStorage.setItem("token", JSON.stringify(res.data.payload));
+      })
+      .catch((err) =>
+        this.setState({
+          ...this.state,
+          error: err.response.data.error,
+        })
+      );
+  };
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.login}>
+          <label htmlFor="username">
+            Username:
+            <input id="username" name="username" onChange={this.handleChange} />
+          </label>
+          <label htmlFor="password">
+            Password:
+            <input
+              id="password"
+              type="password"
+              name="password"
+              onChange={this.handleChange}
+            />
+          </label>
+          <br />
+          <p style={{ color: "red", fontSize: "18px" }}>{this.state.error}</p>
+          <br />
+          <button>Log In</button>
+        </form>
+      </div>
+    );
+  }
+}
+export default Login;
